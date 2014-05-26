51fe37a40f27bf161abe24ce132588a0fbb738b8
Steven Luo <steven+android@steven676.net>
2014-05-26 04:07:34 -0700

    Add licensing information

--- firstrun.sh
+++ firstrun.sh
@@ -7,6 +7,22 @@
 # As of API version 2, command-fd is a file descriptor to which we can write
 # commands to be interpreted by the recovery.
 
+# Copyright (C) 2014 Steven Luo
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 # Ensure we're being used with a supported recovery API version
 recoveryapi="$1"
 [ "$recoveryapi" ] || exit 1
